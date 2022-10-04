8a026327f07958917a6d02e68583331d3ef6bb3a
MD.Emran Alam <105363909+emranCoder@users.noreply.github.com>
2022-10-04 13:03:10 +0600

    html,images,bootstrap
    html, images& some file added

---
+++ contact.php
@@ -0,0 +1,215 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="bootstrap-4.5.2-dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/device-width.css" />
+	<link rel="stylesheet" href="css/flaticon.css" />
+	<link rel="stylesheet" href="css/frontend.min.css" />
+	<link rel="stylesheet" href="css/lightbox.css" />
+	<link rel="stylesheet" href="css/swiper-bundle.min.css" />
+</head>
+<body>
+	<div class="preloder"></div>
+<header>
+
+<nav class="header-nav  navbar-expand-lg navbar-light ">
+<div class="container">
+	<div class="row">
+	<div class="logo">
+		<a href="#"><img src="images/logo.png"></a>
+	</div>
+
+<button class="navbar-toggler menu-active" type="button" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<ul class="header-ul">	
+	<li class="header-li close-menu"><span>x</span></li>
+	<li class="header-li"><a href="index.html">HOME</a></li>
+	<li class="header-li"><a href="services.html">SERVICES</a></li>
+	<li class="header-li"><a href="#">SHOP</a></li>
+	<li class="header-li"><a href="#">PAGES</a></li>
+	<li class="header-li"><a href="about.html">ABOUT US</a></li>
+	<li class="header-li current"><a href="contact.php">CONTACT US</a></li>
+</ul>
+
+
+</div>
+</div>	
+</nav>
+</header>	
+
+<section class="about-us-section contactUs-section">
+	
+<div class="container">
+	<div class="row">
+
+		<div class="col-md-12">
+			<h2 class="about-title">Contact Us</h2>
+		</div>
+	</div>
+</div>
+</section>
+
+<section class="contact-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+			<div class="services-title">
+				<h3>Amazing</h3>
+				<h2>SERVICES PRICING</h2>
+			</div>
+		</div>
+
+<div class="contact-box">
+<div class="container">
+	<div class="row">	
+		<div class="col-md-5 backgreound-color">
+			<div class="story-details">
+				<h3>Our Story</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiselit, sed do eiusmod.</p>
+				<i class="flaticon-phone-call"><p>Angel Beauty, Victoria Street</p></i>
+				<i class="flaticon-mail"><p>+55 654 545 122</p></i>
+				<i class="flaticon-placeholder pin"><p>info @example.com  </p></i>
+			</div>
+		</div>
+
+		<div class="col-md-7">
+			<h2 class="contact-form-title">GET IN TOUCH</h2>
+			<form action="" method="POST" class="contact-form">
+				<label>Your Name (required)</label>
+				<input type="text" name="name">
+				<label>Your Email (required)</label>
+				<input type="email" name="email">
+				<label>Subject</label>
+				<input type="text" name="subject">
+				<label>Your Message</label>
+				<textarea name="message"></textarea>
+				<input class="red-btn send-btn" type="submit" name="submit" value="SEND">
+			</form>
+		</div>
+	</div>
+</div>
+</div>	
+
+		</div>
+	</div>
+</section>
+
+
+<section class="footer-section">
+	<div class="container padding-iteams">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="footer-logo"><img src="images/logo-footer-1.png">
+				</div>
+				<div class="footer-logo-descrive">
+					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa officia.Lorem ipsum dolor sit amet.</p>
+					<p>consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="footer-titel">
+					<h2>Useful Links</h2>
+				</div>
+				<div class="footer-quick-link">
+					<ul>
+						<li class="tab-li"><a href="#">Web Design</a></li>
+						<li class="tab-li"><a href="#">About Angel Beauty Spa</a></li>
+						<li class="tab-li"><a href="#">Mobile</a></li>
+						<li class="tab-li"><a href="#">Why Angel Beauty Spa</a></li>
+						<li class="tab-li"><a href="#">Social Media</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="footer-titel">
+					<h2>Gallery</h2>
+				</div>
+				<div class="footer-gallery">
+					<img src="images/offer-4-1-1-150x150.jpg">
+					<img style="margin-left: 10px;" src="images/offer-2-1-1-150x150.jpg">
+					<img src="images/offer-3-1-1-150x150.jpg">
+					<img style="margin-left: 10px;" src="images/offer-1-1-1-150x150.jpg">
+					<img src="images/home-spa-1-1-150x150.jpg">
+					<img style="margin-left: 10px;" src="images/pricing-1-1-1-150x150.jpg">
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="footer-titel">
+					<h2>Newsletter</h2>
+					<div class="footer-subscribe">
+						<p>Sign up for our mailing list to get latest updates and offers.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<footer>
+	<div class="conatiner">
+		<div class="row">
+			<div class="col-md-7"><p>© 2020 Copyright MD.Emran Alam</p></div>
+			<div class="col-md-5 justify-content-end d-flex">
+				<div class="footer-icon">
+					<i class="flaticon-facebook-app-symbol"></i>
+					<i class="flaticon-twitter"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+	
+	
+	
+
+
+
+
+<script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>	
+<script type="text/javascript" src="bootstrap-4.5.2-dist/js/bootstrap.min.js"></script>	
+<script type="text/javascript" src="bootstrap-4.5.2-dist/js/bootstrap-popper.min.js"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+<script type="text/javascript" src="js/angel.js"></script>
+<script type="text/javascript" src="js/swiper-bundle.min.js"></script>
+
+<script type="text/javascript">
+lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+});
+
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      centeredSlides: true,
+      grabCursor: true,
+      spaceBetween: 5,
+      freeMode: false,
+      loop: true,
+      speed: 1000,
+      Lazy: true,
+      autoplay: {
+    	delay: 4000,
+  	  },
+  	  breakpoints: {
+  	  	// when window width is >= 480px
+  	  	   469: {
+      slidesPerView: 4,
+      spaceBetween: 100,
+    },
+  	  },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+</script>
+</body>
+</html>
